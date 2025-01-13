4002eadfb44262e23788b464b7cebf1f96e798b1
nop <noplab90@gmail.com>
2025-01-14 03:56:32 +0900

    impl render

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use render::Render;
+
 mod render;
 
-fn main() {}
+fn main() {
+    let render = Render::new();
+    render.render(1920, 1080, 4, 2);
+}
--- src/render.rs
+++ src/render.rs
@@ -1,7 +1,5 @@
-use std::intrinsics::powf64;
-
-use intersection::Intersection;
-use material::Color;
+use material::{Color, IOR};
+use ppm::save_ppm;
 use random::XorShiftRandom;
 use ray::Ray;
 use scene::Scene;
@@ -29,15 +27,13 @@ pub struct Render {
 }
 
 impl Render {
-    fn new() -> Render {
+    pub fn new() -> Render {
         Render {
             scene: Scene::new(),
         }
     }
 
-    fn render(&self, width: u32, height: u32) {
-        let mut random = XorShiftRandom::new(0);
-
+    pub fn render(&self, width: u32, height: u32, samples: u32, supersamples: u32) {
         let camera_pos = Vec3::new(50.0, 52.0, 220.0);
         let camera_dir = Vec3::new(0.0, -0.04, -1.0).normalize();
         let camera_up = Vec3::new(0.0, 1.0, 0.0);
@@ -54,42 +50,56 @@ impl Render {
         let mut image = vec![Vec3::new(0.0, 0.0, 0.0); (width * height) as usize];
 
         for y in 0..height {
+            println!("Rendering (y = {} / {})", y, height);
+
+            let mut rnd = XorShiftRandom::new(y + 1 as u32);
+
             for x in 0..width {
-                let mut color = Vec3::new(0.0, 0.0, 0.0);
+                let image_index = (height - y - 1) * width + x;
 
-                for sy in 0..2 {
-                    for sx in 0..2 {
-                        let mut subpixel_color = Vec3::new(0.0, 0.0, 0.0);
+                for sy in 0..supersamples {
+                    for sx in 0..supersamples {
+                        let mut accumulated_radiance = Color::new(0.0, 0.0, 0.0);
 
-                        for _ in 0..4 {
-                            let ray_dir = screen_center
-                                + screen_x * ((sx as f64 + 0.25 + 0.5 * random.next()) / 2.0 - 0.5)
-                                + screen_y * ((sy as f64 + 0.25 + 0.5 * random.next()) / 2.0 - 0.5)
-                                - camera_pos;
-                            let ray_dir = ray_dir.normalize();
+                        for s in 0..samples {
+                            let rate = (s as f64 + 1.0) / samples as f64;
+                            let r1 = sx as f64 * rate / 2.0;
+                            let r2 = sy as f64 * rate / 2.0;
 
-                            subpixel_color = subpixel_color
-                                + self.radiance(&camera_pos, &ray_dir, &mut random, 0);
+                            let screen_position = screen_center
+                                + screen_x * ((r1 + x as f64) / width as f64 - 0.5)
+                                + screen_y * ((r2 + y as f64) / height as f64 - 0.5);
+
+                            let dir = (screen_position - camera_pos).normalize();
+
+                            accumulated_radiance = accumulated_radiance
+                                + self.radiance(
+                                    &Ray {
+                                        origin: camera_pos,
+                                        direction: dir,
+                                    },
+                                    &mut rnd,
+                                    0,
+                                );
                         }
-
-                        subpixel_color = subpixel_color * 0.25;
-                        color = color + subpixel_color;
+                        image[image_index as usize] = image[image_index as usize]
+                            + accumulated_radiance / (samples * supersamples * supersamples) as f64;
                     }
                 }
-
-                image[(height - y - 1) as usize * width as usize + x as usize] = color;
             }
         }
+
+        save_ppm("image.ppm", &image, width, height);
     }
 
-    fn radiance(&self, ray: &Ray, rnd: XorShiftRandom, depth: u32) -> Color {
+    fn radiance(&self, ray: &Ray, rnd: &mut XorShiftRandom, depth: u32) -> Color {
         if let Some(intersection) = self.scene.intersect(ray) {
             let hit_point = intersection.hit_point;
             let object_id = intersection.object_id;
 
             let sphere = &self.scene.spheres()[object_id as usize];
             let hitpoint = &intersection.hit_point;
-            let orienting_normal = if hitpoint.normal.dot(ray.dir) < 0.0 {
+            let orienting_normal = if hitpoint.normal.dot(ray.direction) < 0.0 {
                 hitpoint.normal
             } else {
                 hitpoint.normal * -1.0
@@ -97,11 +107,117 @@ impl Render {
             let mut russian_roulette_probability = sphere.color.max();
 
             if depth > DEPTH_LIMIT {
-                russian_roulette_probability *= powf64(0.5, depth as f64 - DEPTH_LIMIT as f64);
+                russian_roulette_probability *= 0.5f64.powf(depth as f64 - DEPTH_LIMIT as f64);
             }
 
             if depth > DEPTH {
-                if (rnd.next())
+                if rnd.next_f64() >= russian_roulette_probability {
+                    return sphere.emission;
+                }
+            } else {
+                russian_roulette_probability = 1.0;
+            }
+
+            let incoming_radiance;
+            let weight;
+
+            match sphere.reflection_type {
+                material::RefrectionType::Diffuse => {
+                    let w = orienting_normal;
+                    let u = if w.x.abs() > 0.1 {
+                        Vec3::new(0.0, 1.0, 0.0)
+                    } else {
+                        Vec3::new(1.0, 0.0, 0.0)
+                    };
+                    let v = w.cross(u);
+
+                    let r1 = 2.0 * std::f64::consts::PI * rnd.next_f64();
+                    let r2 = rnd.next_f64();
+                    let r2s = r2.sqrt();
+                    let dir = Vec3::new(r1.cos() * r2s, r1.sin() * r2s, (r2.sqrt()).sqrt());
+
+                    incoming_radiance = self.radiance(
+                        &Ray {
+                            origin: hitpoint.position,
+                            direction: u * dir.x + v * dir.y + w * dir.z,
+                        },
+                        rnd,
+                        depth + 1,
+                    );
+                    weight = sphere.color / russian_roulette_probability;
+                }
+                material::RefrectionType::Specular => {
+                    incoming_radiance = self.radiance(
+                        &Ray {
+                            origin: hitpoint.position,
+                            direction: ray.direction
+                                - hitpoint.normal * 2.0 * hitpoint.normal.dot(ray.direction),
+                        },
+                        rnd,
+                        depth + 1,
+                    );
+                    weight = sphere.color / russian_roulette_probability;
+                }
+                material::RefrectionType::Refraction => {
+                    let refrection_ray = Ray::new(hit_point.position, ray.direction);
+                    let into = hitpoint.normal.dot(orienting_normal) > 0.0;
+
+                    let nc = 1.0;
+                    let nt = IOR;
+                    let nnt = if into { nc / nt } else { nt / nc };
+                    let ddn = ray.direction.dot(orienting_normal);
+                    let cos2t = 1.0 - nnt * nnt * (1.0 - ddn * ddn);
+
+                    if cos2t < 0.0 {
+                        incoming_radiance = self.radiance(&refrection_ray, rnd, depth + 1);
+                        weight = sphere.color / russian_roulette_probability;
+                    } else {
+                        let refrection_ray = Ray::new(
+                            hitpoint.position,
+                            (ray.direction * nnt
+                                - hitpoint.normal
+                                    * (if into { 1.0 } else { -1.0 })
+                                    * (ddn * nnt + cos2t.sqrt()))
+                            .normalize(),
+                        );
+
+                        let a = nt - nc;
+                        let b = nt + nc;
+                        let r0 = (a * a) / (b * b);
+
+                        let c = 1.0
+                            - (if into {
+                                -ddn
+                            } else {
+                                refrection_ray.direction.dot(hitpoint.normal)
+                            });
+                        let re = r0 + (1.0 - r0) * c.powi(5);
+                        let nnt2 = (if into { nt / nc } else { nc / nt }).powi(2);
+                        let tr = (1.0 - re) * nnt2;
+
+                        let probability = 0.25 + 0.5 * re;
+                        if depth > 2 {
+                            if rnd.next_f64() < probability {
+                                incoming_radiance =
+                                    self.radiance(&refrection_ray, rnd, depth + 1) * re;
+                                weight =
+                                    sphere.color / (probability * russian_roulette_probability);
+                            } else {
+                                incoming_radiance =
+                                    self.radiance(&refrection_ray, rnd, depth + 1) * tr;
+                                weight = sphere.color
+                                    / ((1.0 - probability) * russian_roulette_probability);
+                            }
+                        } else {
+                            incoming_radiance =
+                                self.radiance(&refrection_ray, rnd, depth + 1) * (re + tr);
+                            weight = sphere.color / russian_roulette_probability;
+                        }
+                    }
+                }
+            }
+
+            return sphere.emission + incoming_radiance * weight;
         } else {
             BACKGROUND_COLOR
         }
