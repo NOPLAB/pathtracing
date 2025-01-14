06058330b22a27cc3b249a61892c2645dd24ab51
yanorei32 <yanorei@hotmail.co.jp>
2025-01-14 11:21:06 +0900

    Use num_cpus instead of constant 16 for worker preparation

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,6 @@ mod render;
 
 fn main() {
     let render = Render::new();
-    render.render(640, 480, 10, 5, 16);
+    println!("{} CPUs detected", num_cpus::get());
+    render.render(640, 480, 10, 5, num_cpus::get() as u32);
 }
