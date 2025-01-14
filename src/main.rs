cee26c38b8e49408858d832bd22724164d622521
yanorei32 <yanorei@hotmail.co.jp>
2025-01-14 11:45:28 +0900

    Implement ENV (WORKERS) based worker count (Closes #2)

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,6 @@ mod render;
 
 fn main() {
     let render = Render::new();
-    render.render(640, 480, 10, 5, 16);
+    let worker_count = std::env::var("WORKERS").map(|s| s.parse().expect("Failed to parse env WORKERS")).unwrap_or(16);
+    render.render(640, 480, 10, 5, worker_count);
 }
